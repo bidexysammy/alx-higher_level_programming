eed65dc22ea5362ed0ab36683e2e732eb3a0ea32
bidexysammy <bidexysammy@yahoo.com>
2023-02-16 13:34:09 -0800

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates the Mysql server user user_0d_1 and grant all privileges.
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PROVILEGES ON *.* TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
