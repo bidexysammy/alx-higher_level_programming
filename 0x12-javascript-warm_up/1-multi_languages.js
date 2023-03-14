791347b8b34211138b7c2c671bf721e72e6ae326
bidexysammy <bidexysammy@yahoo.com>
2023-03-14 19:59:58 +0000

    JavaScript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-const string = 'C is fun\nPython is cool\nJavascript is amazing';
-console.log(string);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
