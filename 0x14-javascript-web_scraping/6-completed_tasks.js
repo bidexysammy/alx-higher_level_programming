e7ff557d61ffd17925dec79efd4fc43587638f9c
abidemiolayiwola23 <abidemiolayiwola23@gmail.com>
2023-04-25 21:51:51 +0000

    web_scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,18 @@
 #!/usr/bin/node
-const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
+
+const r = require('request');
+
+r.get(process.argv[2], (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    const computed = {};
     const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+    todos.forEach(task => {
+      if (task.completed) {
+        if (!computed[task.userId]) computed[task.userId] = 1;
+        else computed[task.userId] += 1;
       }
     });
-    console.log(completed);
+    console.log(computed);
   }
 });
